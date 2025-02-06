112ab488f62ce90cf56f10c0acdc2fae2df427af
Çağıl Alsaç <cagilalsac@outlook.com>
2025-02-06 12:23:23 +0300

    APP Project created with Domain including Tag Entity. DbSet for Tag created in ProjectsDb DbContext Class and database connection defined.

---
+++ APP/Domain/ProjectsDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APP.Domain
+{
+    public class ProjectsDb : DbContext
+    {
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb;database=PMSCTISProjectsDB;trusted_connection=true;"));
+        }
+    }
+}
---
+++ APP/Domain/Tag.cs
@@ -0,0 +1,12 @@
+﻿using CORE.APP.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace APP.Domain
+{
+    public class Tag : Entity
+    {
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+    }
+}
